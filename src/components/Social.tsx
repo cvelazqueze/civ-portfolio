dfbfba39f572d3518b87d6c1c233916362499b64
Velz <velz@Velzs-MacBook-Pro.local>
2024-07-01 22:42:44 -0600

    Initial

---
+++ src/components/Social.tsx
@@ -0,0 +1,23 @@
+import { Link } from "lucide-react";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+
+const socials = [
+  { icon: <FaGithub />, path: "https://github.com/cvelazqueze" },
+  { icon: <FaLinkedinIn />, path: " " },
+];
+
+const Social = ({ containerStyles, iconStyles }: any) => {
+  return (
+    <div className={containerStyles}>
+      {socials.map((item, index) => {
+        return (
+          <Link key={index} href={item.path} className={iconStyles}>
+            {item.icon}
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Social;
